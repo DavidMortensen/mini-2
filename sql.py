3bb43eb7e6ad4bc59a688954e5115249bdf4e074
Nicolai Rendtslev <nre@caliodesign.com>
2018-03-12 18:25:21 +0100

    answered query 1 through 3 and added template for query 4

--- sql.py
+++ sql.py
@@ -1,3 +1,4 @@
+""" Initialize all files"""
 from pyspark.sql import SQLContext
 from pyspark.sql.types import *
 from pyspark.sql import functions as F
@@ -5,6 +6,7 @@ from pyspark.sql import functions as F
 sqlContext=SQLContext(sc)
 #loads in all files
 df = spark.read.format("csv").option('header','true').load("../chicago-taxi-rides-2016/chicago_taxi_trips_2016_*.csv")
+df_drivers = spark.read.format("csv").option('header','true').load("../chicago_taxi_drivers.csv") 
 
 #df.show()
 #df.dtypes()
@@ -14,6 +16,25 @@ df.select("trip_total").show()
 #df.count()
 #df.schema
 
+"""Query 1 """"
 #sum of column "trip_total"
+dt = df
 total = dt.groupBy().agg(F.sum("trip_total")).collect()
 total
+
+"""Query 2"""
+company_total_df = df.withColumn("trip_total",df["trip_total"].cast('float'))
+ #groups by company and sums the value trip_total in the new column individual_trip_total
+company_total_df.groupBy("company").agg(F.sum("trip_total").alias("individual_trip_total")).show()  
+
+"""Query 3"""
+payment_type_df = df.withColumn("trip_total",df["trip_total"].cast('float'))
+ #groups by company and sums the value trip_total in the new column individual_trip_total
+payment_type_df.groupBy("payment_type").agg(F.sum("trip_total").alias("payment_type_total")).show() 
+
+
+
+"""Query 4"""
+company_11_df.groupBy("id", "company").show()
+
+

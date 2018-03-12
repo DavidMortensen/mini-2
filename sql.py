681e3b504ebd99226fecf5e845a081e8e7ff50a8
Frederik Jensen <fredefut@hotmail.com>
2018-03-12 17:21:49 +0100

    Question 1 solution

--- sql.py
+++ sql.py
@@ -3,15 +3,17 @@ from pyspark.sql.types import *
 from pyspark.sql import functions as F
 
 sqlContext=SQLContext(sc)
-
+#loads in all files
 df = spark.read.format("csv").option('header','true').load("chicago_taxi_trips_2016*.csv")
 
 #df.show()
 #df.dtypes()
+#selects the column trip_total
 df.select("trip_total").show()
 #df.head()
 #df.count()
 #df.schema
 
+#sum of column "trip_total"
 total = dt.groupBy().agg(F.sum("trip_total")).collect()
 total

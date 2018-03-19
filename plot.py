dcfddd46f20cfc7a6f87f35e68dcaedd3f6c4a78
Frederik Jensen <fredefut@hotmail.com>
2018-03-19 11:17:49 +0100

    Plot

--- plot.py
+++ plot.py
@@ -1,9 +1,12 @@
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-Python = [1,3,9,2,3,5,9,11]
-SQL= [9,2,3,4,5,6,7,3,2]
-RDD= [3,2,5,3,6,8,9,0,1]
+Python = [0.2]
+RDD= [5.0, 21.0, 51.0, 39.4]
+SQL= [3.8,10.6,24.3, 80.0]
+
+x=[1,4,8,12]
+
 
 with PdfPages('plot.pdf') as pdf:
     plt.figure(figsize=(7,7))
@@ -12,7 +15,7 @@ with PdfPages('plot.pdf') as pdf:
     plt.plot(RDD, color="yellow")
     plt.title("Title")
     plt.ylabel("ylabel")
-    plt.axis([0,10,0,15])
+    plt.axis([0,12, 0, 120])
     plt.xlabel("xlabel")
     plt.legend(["Python", "SQL", "RDD"], loc='upper right')
     pdf.savefig()

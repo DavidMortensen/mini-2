a6037ca9a3c381e1e3c8fd122b53865fc06c6286
Nicolai Rendtslev <nre@caliodesign.com>
2018-03-21 10:50:51 +0100

    commenting all but uncached chicago.py

--- rdd.py
+++ rdd.py
@@ -1,9 +1,9 @@
 from pyspark import SparkContext, SparkConf
 import csv
 sc = SparkContext (master = 'local[1]')
-
+# define which directory we want to load our rides from
 data_temp = sc.textFile("../chicago-taxi-rides-2016")
-
+# define which file we want to load or drivers from
 taxi_drivers = sc.textFile("../chicago_taxi_drivers.csv")
 taxi_drivers = taxi_drivers.map(lambda line: line.split(','))
 
@@ -41,21 +41,27 @@ Index values:
   """
   
 #query 1 - sum trips:
+# we index on column 13 where we have the value trip_total
 total_sum = data.map(lambda x: x[13]).filter(lambda x: len(x) != 0)
 total_sum = total_sum.map(lambda x: float(x))
 print(total_sum.sum())
 
 #query 2 - sum company turnover
+# we first filter our data to remove all 0 values from both company and trip total
 data_filtered = data.filter(lambda x: len(x[15]) != 0)
 data_filtered = data_filtered.filter(lambda x: len(x[13]) != 0)
+# assign amount paid from column 13 and convert to float throughout
 amount = data_filtered.map(lambda x: x[13]).map(lambda x: float(x))
 company = data_filtered.map(lambda x: x[15])
+# create tuple with company and amount in each tuple
 joined = company.zip(amount)
+# group by company so we sum the amount paid in each company respectively
 y = joined.reduceByKey(lambda x,y: x+y)
 y.take(10)
 
 #query 3 - sum cash trips:
 cash = data.filter(lambda x: x[14] == 'Cash')
+# remove all rows that have 0 in our trip_total column
 cash_payments = cash.map(lambda x: x[13]).filter(lambda x: len(x) != 0)
 cash_payments = cash_payments.map(lambda x: float(x))
 print(cash_payments.sum())
--- sql.py
+++ sql.py
@@ -5,57 +5,39 @@ from pyspark.sql import functions as F
 
 sqlContext=SQLContext(sc)
 
-#loads in all files
-df = spark.read.format("csv").option('header','true').load("../chicago-taxi-rides-2016/chicago_taxi_trips_2016_*.csv")
-df_drivers = spark.read.format("csv").option('header','true').load("../chicago_taxi_drivers.csv") 
+# loads in all files
+df = spark.read.format("csv").option('header','true').load("../4/chicago_taxi_trips_2016_*.csv")
+# load in drivers without removing header, we'll have to rename a column before use in query 4
+df_drivers = spark.read.format("csv").option('header','false').load("../chicago_taxi_drivers.csv") 
 
-
-#df.show()
-#df.dtypes()
-#selects the column trip_total
 df.select("trip_total").show()
-#df.head()
-#df.count()
-#df.schema
 
 """Query 1 """
-#sum of column "trip_total"
+# sum of column "trip_total"
 dt = df
 total = dt.groupBy().agg(F.sum("trip_total")).collect()
 total
 
 """Query 2"""
+# define our column and convert to float so we can sum
 company_total_df = df.withColumn("trip_total",df["trip_total"].cast('float'))
- #groups by company and sums the value trip_total in the new column individual_trip_total
+# groups by company and sums the value trip_total in the new column individual_trip_total
 company_total_df.groupBy("company").agg(F.sum("trip_total").alias("individual_trip_total")).show()  
 
 """Query 3"""
+# define our column and convert to float so we can sum
 payment_type_df = df.withColumn("trip_total",df["trip_total"].cast('float'))
- #groups by company and sums the value trip_total in the new column individual_trip_total
+# groups by company and sums the value trip_total in the new column individual_trip_total
 payment_type_df.groupBy("payment_type").agg(F.sum("trip_total").alias("payment_type_total")).show() 
 
-
-
 """Query 4"""
-#reads in the driver file
-drivers=spark.read.option('header','false').csv("../chicago_taxi_drivers.csv")
+# reads in the driver file
+drivers = df_drivers
 drivers=drivers.withColumnRenamed("_c0", "taxi_id")
 #joining driver file with dataframe
 total_df = dt.join(drivers, on=['taxi_id'], how = 'left_outer')
-#new column name
+# new column name
 total_df=total_df.withColumnRenamed("_c1", "driver_name")
-#selecting distinct values
+# selecting distinct values
 company_11=total_df.where("company == 11").select(["company", "driver_name"]).distinct()
 company_11.show()
-company_11.count()
-
-
-
-#company_11=spark.sql("SELECT DISTINCT driver_name, company FROM total_df WHERE company='11'")
-#company_11.show()
-#company_11.count() #7 drivers from company 11
-
-
-
-
-
